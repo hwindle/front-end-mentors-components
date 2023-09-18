5ecf220f8eece3d0b38f07967f62b39db1ae4ec1
Hazel Windle <hwindle83@googlemail.com>
2023-09-18 22:30:09 +0100

    finished tailwind css and media queries

---
+++ 02_product_preview_card/tailwind.config.js
@@ -0,0 +1,19 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./**/*.{html,js}"],
+  theme: {
+    colors: {
+      'leaf-green': 'hsl(158, 36%, 37%)',
+      'cream': 'hsl(30, 38%, 92%)',
+      'black': 'hsl(212, 21%, 14%)',
+      'grey': 'hsl(228, 12%, 48%)',
+    },
+    fontFamily: {
+      'sans': 'Montserrat',
+      'serif': 'Fraunces',
+    },
+    extend: {},
+  },
+  plugins: [],
+}
+
