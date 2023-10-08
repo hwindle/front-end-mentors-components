e48d7b5ecf64c00fb395f4e19ae9e4278f4b0b51
Hazel Windle <hwindle83@googlemail.com>
2023-10-08 14:08:18 +0100

    Altered base url again

--- 03_interactive_rating_component/vite-project/vite.config.js
+++ 03_interactive_rating_component/vite-project/vite.config.js
@@ -3,6 +3,6 @@ import react from '@vitejs/plugin-react'
 
 // https://vitejs.dev/config/
 export default defineConfig({
-  base: '/front-end-mentors-components/03_interactive_rating_component/vite-project/dist',
+  base: 'https://splendid-swan-2aea5a.netlify.app/03_interactive_rating_component/vite-project/dist/',
   plugins: [react()],
 })
